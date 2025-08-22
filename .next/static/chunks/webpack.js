b201244c39ebf670cc2f962c29498d381bc2949f
kamunity1 <mikefuller76@outlook.com>
2025-08-23 06:04:01 +0800

    feat: scaffold Next.js app pages and routing structure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff017bbd9a5876dc"; }
+/******/ 		__webpack_require__.h = function() { return "82af730b5b77e17b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
